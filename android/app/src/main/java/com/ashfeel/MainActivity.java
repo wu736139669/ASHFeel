088a31b252c73befa69511bb1cf6e1b67f7653b8
QuDian <wushenghua@qudian.com>
2018-11-20 22:23:15 +0800

    first commit

---
+++ android/app/src/main/java/com/ashfeel/MainActivity.java
@@ -0,0 +1,24 @@
+package com.ashfeel;
+
+import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationReactPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.qudian.FixTextView.RNFixTextVieWPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.psykar.cookiemanager.CookieManagerPackage;
+import com.microsoft.codepush.react.CodePush;
+import com.qudian.ProgressHUD.RNProgressHudPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ASHFeel";
+    }
+}
---
+++ ios/Pods/Target Support Files/Pods-ASHFeel/Pods-ASHFeel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASHFeel : NSObject
+@end
+@implementation PodsDummy_Pods_ASHFeel
+@end
